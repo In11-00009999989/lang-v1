20f6fbf7d8d509abe74a20edbbef8257dce6b16d
Grigorii Horos <horosgrisa@gmail.com>
2020-03-03 22:20:22 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from lib.node import Node
+from lib.bin_tree import arr_to_tree
+
+
+lineList = list()
+with open("./program.ze") as f:
+    for line in f:
+        lineList.append(line)
+
+for operations in lineList:
+
+    arr_operations = (
+        operations.replace("(", "").replace(")", "").replace("\n", "").split(" ")
+    )
+    filter(None, arr_operations)
+
+    tree, _ = arr_to_tree(arr_operations)
+
+    tree.calc()
